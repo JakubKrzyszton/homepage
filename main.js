4820b3d1b03fdd8f19297c741818fcdc986a7209
Jakub Krzyszton <krzyszton.jakub@gmail.com>
2020-02-02 13:54:37 +0100

    BEM and querySelector

--- main.js
+++ main.js
@@ -2,3 +2,11 @@ console.log('Siemano!');
 const name = 'Kuba';
 const age = 28;
 console.log(`Mam na imię ${name} i mam ${age} lat.`)
+
+const mainHeader = document.querySelector('.mainHeader')
+console.log(mainHeader);
+
+const paragraphs = document.querySelectorAll('p')
+console.log(paragraphs);
+
+console.log(paragraphs[0]);
